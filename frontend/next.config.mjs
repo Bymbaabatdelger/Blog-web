a84c32be866e3ddfe1fbbb0750f4ea2d53742f3d
Bymbaabatdelger <145735499+Bymbaabatdelger@users.noreply.github.com>
2024-10-21 00:25:34 +0800

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "res.cloudinary.com" ,
-          },
-        ],
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
-      env: {
-        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "ddjcpzprt",
-      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "ddjcpzprt",
+  },
+  output: "export",
 };
 
 export default nextConfig;
